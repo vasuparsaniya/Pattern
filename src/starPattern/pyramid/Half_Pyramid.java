a72783a695e5285fe812ed921be46b1d156e1917
vasu <vasu@gmail.com>
2023-07-27 15:42:37 +0530

    Half Pyramid

--- src/starPattern/pyramid/Half_Pyramid.java
+++ src/starPattern/pyramid/Half_Pyramid.java
@@ -1,10 +1,20 @@
 package starPattern.pyramid;
 
+import java.util.Scanner;
+
 public class Half_Pyramid {
-
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int i, j, row;
+		System.out.println("Enter the number of Row:");
+		Scanner r=new Scanner(System.in);
+		row=r.nextInt();
+		
+		for(i=1;i<=row;i++) {
+			for(j=1;j<=i;j++) {
+				System.out.print("*");
+			}
+			System.out.println("");
+		}
 	}
-
 }
